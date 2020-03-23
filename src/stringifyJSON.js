2f7525629825121700be1aa73bfe80393889600e
Brian de Regt <brian.deregt@gmail.com>
2020-03-23 15:31:55 -0400

    lunch break, w/ pomander fails, will fix in pm session

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,22 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = "";
+
+  var helper = function(ele){
+    if (Array.isArray(ele)){
+      result += "[" + helper(ele) + "]";
+    } else if (typeof ele === 'string'){
+      result += '"' + ele + '"';
+    } else if (typeof ele === 'number' || typeof ele === 'boolean'){
+      result += ele.toString();
+    } else if (ele === null){
+      result += "null";
+    } else if (typeof ele === 'function' || ele === undefined){
+
+    }
+  }
+  helper(obj);
+
+  return result;
 };
