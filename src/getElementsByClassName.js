37966c16d8825d9147e4cee078b4149520dc6102
Brian de Regt <brian.deregt@gmail.com>
2020-03-23 19:49:52 -0400

    all spec tests passing

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var result = [];
+  var helper = function(ele){
+    if (ele.childNodes.length > 0){
+      for (var i = 0; i < ele.childNodes.length; i++){
+        helper(ele.childNodes[i]);
+      }
+    }
+    if (ele.classList !== undefined){
+      if (ele.classList.contains(className)){
+        result.unshift(ele);
+      }
+    }
+  }
+  helper(document.body);
+  return result;
 };
+
+
+//You should use document.body, element.childNodes, and element.classList
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -17,7 +17,9 @@ var stringifyJSON = function(obj) {
     result += '[';
     for (var i = 0; i < obj.length; i++) {
       result += stringifyJSON(obj[i]);
-      if (i < obj.length - 1) result += ',';
+      if (i < obj.length - 1){
+        result += ',';
+      }
     }
     result += ']';
   } else {
@@ -28,46 +30,13 @@ var stringifyJSON = function(obj) {
       if (typeof obj[key] === 'function'){
         break;
       } else {
-      result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+        result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
       }
     }
-    if (result.length > 1) result = result.substring(0, result.length - 1);
+    if (result.length > 1) {
+      result = result.substring(0, result.length - 1);
+    }
     result += '}';
   }
   return result;
 };
-
-
-// var helper = function(ele){
-  //   if (Array.isArray(ele)){
-  //     if(ele.length === 0){
-  //       result += "[]";
-  //     } else {
-  //       for (var i = 0; i < ele.length; i++){
-  //         if (i === 0){
-  //           result += '['
-  //         }
-  //         result += helper(ele[i]);
-  //         if (i !== ele.length -1){
-  //           result += ','
-  //         }
-  //         if (i === ele.length -1){
-  //           result += ']';
-  //         }
-  //       }
-  //     }
-  //   } else if (typeof ele === 'string'){
-  //     result += '"' + ele + '"';
-  //   } else if (typeof ele === 'number'){
-  //     return result += ele;
-  //   } else if(typeof ele === 'boolean'){
-  //     result += ele;
-  //   } else if (ele === null){
-  //     result += "null";
-  //   } else if (typeof ele === 'function' || ele === undefined){
-  //     return undefined;
-  //   } else {
-  //     result += "{" + helper(ele) + "}";
-  //   }
-  // };
-  // helper(obj);
